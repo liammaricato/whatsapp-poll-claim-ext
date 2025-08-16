baf9fd5f32a3a99077d17450818e5eaf216db862
Liam Maricato <liammaricato@gmail.com>
2025-08-15 23:42:48 -0300

    Add app basic functionality

--- main.js
+++ main.js
@@ -1,60 +1,69 @@
-let conversationEls = document.querySelectorAll('div#pane-side div[aria-colindex="2"]')
+const conversationEls = document.querySelectorAll('div#pane-side div[aria-colindex="2"]')
+let showBubbleContent = false
+let claimNextPoll = false
 
-const extBubbleEl = document.createElement('div')
-extBubbleEl.id = 'claim-ext-bubble'
-const extBubbleContentEl = document.createElement('div')
-extBubbleContentEl.id = 'claim-ext-bubble-content'
-extBubbleContentEl.style.display = 'none'
-extBubbleContentEl.innerHTML = `
-    <h3>Claim Extension</h3>
+const injectBubbleEl = document.createElement('div')
+injectBubbleEl.id = 'claim-ext-bubble'
+injectBubbleEl.innerHTML = `
+    <img src="https://static.thenounproject.com/png/1103032-200.png" id="claim-ext-bubble-icon">
+    <div id="claim-ext-bubble-content">
+        <h3>Claim Extension</h3>
+        <div id="claim-ext-bubble-content-body">
+            <button id="claim-poll-btn">Claim next poll</button>
+        </div>
+    </div>
 `
-extBubbleEl.appendChild(extBubbleContentEl)
-extBubbleEl.addEventListener('click', () => {
-    extBubbleContentEl.style.display = 'block'; // toggle
+
+document.body.appendChild(injectBubbleEl)
+
+const bubbleEl = document.getElementById('claim-ext-bubble')
+const bubbleContentEl = document.getElementById('claim-ext-bubble-content')
+const claimPollBtn = document.getElementById('claim-poll-btn')
+
+bubbleEl.addEventListener('click', () => {
+    if (showBubbleContent) {
+        bubbleContentEl.style.display = 'none'
+    } else {
+        bubbleContentEl.style.display = 'flex'
+    }
+    showBubbleContent = !showBubbleContent
 })
 
-const claimPollBtn = document.createElement('button')
-claimPollBtn.id = 'claim-poll-btn'
-claimPollBtn.innerHTML = 'Claim next poll'
-claimPollBtn.addEventListener('click', () => {
-    console.log('Claim next poll')
+claimPollBtn.addEventListener('click', (event) => {
+    event.stopPropagation()
+    
+    if (!claimNextPoll) {
+        console.log('Claim next poll')
+        claimNextPoll = true
+    } else {
+        console.log('Cannot claim next poll')
+        claimNextPoll = false
+    }
+
+    injectBubbleEl.classList.toggle('active')
 })
-extBubbleContentEl.appendChild(claimPollBtn)
 
-document.body.appendChild(extBubbleEl)
+// Set up MutationObserver to watch for new elements c
+const observer = new MutationObserver((mutations) => {
+    mutations.forEach((mutation) => {
+        if (!claimNextPoll) return
 
-
-// Set up MutationObserver to watch for chat settings popover
-// const observer = new MutationObserver((mutations) => {
-//     mutations.forEach((mutation) => {
-//         mutation.addedNodes.forEach((node) => {
-//             // Check if the added node is an element
-//             if (node.nodeType === Node.ELEMENT_NODE) {
-//                 // if (node.matches('.chat-settings__popover') || node.querySelector('.chat-settings__popover')) {
-//                 //     // Small delay to ensure the popover content is fully rendered
-//                 //     setTimeout(injectToggle, 100)
-//                 // }
-
-//                 // if (node.matches('.reward-center__content__with-bits-rewards') || node.querySelector('.reward-center__content__with-bits-rewards')) {
-//                 //     setTimeout(injectPointsData, 100)
-//                 // }
-
-//                 // if (node.matches('strong.community-points-summary__points-add-text') || node.querySelector('strong.community-points-summary__points-add-text')) {
-//                 //     const points = node.textContent.match(/\d+/)[0]
-//                 //     updatePointsData(parseInt(points))
-//                 // }
-
-//                 // Adblocker
-//                 // 'iframe#amazon-video-ads-in-stream-iframe'
-//                 // document.querySelector('button[data-a-target="player-mute-unmute-button"]').click()
-//                 // document.querySelector('.picture-by-picture-player video')
-//             }
-//         })
-//     })
-// })
+        mutation.addedNodes.forEach((node) => {
+            if (node.nodeType === Node.ELEMENT_NODE) {
+                if (node.querySelector('span[data-icon="multi-select-icon-filled"]')) {
+                    setTimeout(() => {
+                        node.querySelector('input[type="checkbox"]').click()
+                        claimNextPoll = false
+                        injectBubbleEl.classList.toggle('active')
+                    }, 100)
+                }
+            }
+        })
+    })
+})
 
 // Start observing the document for changes
-// observer.observe(document.body, {
-//     childList: true,
-//     subtree: true
-// })
+observer.observe(document.body, {
+    childList: true,
+    subtree: true
+})
